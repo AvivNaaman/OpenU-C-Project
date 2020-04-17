3fee099aafe9256180474fd64dac67c63cab9990
Aviv Naaman <avivnaaman04@gmail.com>
2020-04-17 14:07:59 +0300

    DONE:  1.12

--- code.c
+++ code.c
@@ -1,7 +1,92 @@
+#include <stdio.h>
+#include <string.h>
 #include "code.h"
+#include "utils.h"
 
-static *char
 
 int process_code(char *line, int i) {
+	char temp[80];
+	opcode curr_opcode;
+	funct curr_funct;
+	code_word *curr_code; /* current code word */
+	int j;
+	/* Skip white chars */
+	MOVE_TO_NOT_WHITE(line, i)
 
+	for (j = 0;line[i] && line[i] != '\t' && line[i] != ' ' && line[i] != '\n' && line[i] != EOF; i++,j++) {
+		temp[j] = line[i];
+	}
+	temp[j] = '\0'; /* End of string */
+
+	/* Get opcode & funct by command name into curr_opcode/curr_funct */
+	get_opcode_func(temp, &curr_opcode, &curr_funct);
+	/* If invalid operation, print and skip processing the line. */
+	if (curr_opcode == NONE_OP) {
+		printf("Unrecognized instruction: %s.", temp);
+		return TRUE;
+	}
+
+
+}
+
+void get_opcode_func(char* cmd, opcode *opcode_out, funct *funct_out) {
+	*funct_out = NONE_FUNCT;
+	if (strcmp(cmd, "mov") == 0) {
+		*opcode_out = MOV_OP;
+	}
+	else if (strcmp(cmd, "cmp") == 0) {
+		*opcode_out = CMP_OP;
+	}
+	else if (strcmp(cmd, "add") == 0) {
+		*opcode_out = ADD_OP;
+		*funct_out = ADD_FUNCT;
+	}
+	else if (strcmp(cmd, "sub") == 0) {
+		*opcode_out = SUB_OP;
+		*funct_out = SUB_FUNCT;
+	}
+	else if (strcmp(cmd, "lea") == 0) {
+		*opcode_out = LEA_OP;
+	}
+	else if (strcmp(cmd, "clr") == 0) {
+		*opcode_out = CLR_OP;
+		*funct_out = CLR_FUNCT;
+	}
+	else if (strcmp(cmd, "not") == 0) {
+		*opcode_out = NOT_OP;
+		*funct_out = NOT_FUNCT;
+	}
+	else if (strcmp(cmd, "inc") == 0) {
+		*opcode_out = INC_OP;
+		*funct_out = INC_FUNCT;
+	}
+	else if (strcmp(cmd, "dec") == 0) {
+		*opcode_out = DEC_OP;
+		*funct_out = DEC_FUNCT;
+	}
+	else if (strcmp(cmd, "jmp") == 0) {
+		*opcode_out = JMP_OP;
+		*funct_out = JMP_FUNCT;
+	}
+	else if (strcmp(cmd, "bne") == 0) {
+		*opcode_out = BNE_OP;
+		*funct_out = BNE_FUNCT;
+	}
+	else if (strcmp(cmd, "jsr") == 0) {
+		*opcode_out = JSR_OP;
+		*funct_out = JSR_FUNCT
+	}
+	else if (strcmp(cmd, "red") == 0) {
+		*opcode_out = RED_OP;
+	}
+	else if (strcmp(cmd, "prn") == 0) {
+		*opcode_out = PRN_OP;
+	}
+	else if (strcmp(cmd, "rts") == 0) {
+		*opcode_out = RTS_OP;
+	}
+	else if (strcmp(cmd, "stop") == 0) {
+		*opcode_out = STOP_OP;
+	}
+	else *opcode_out = NONE_OP; /* Not found! */
 }
--- code.h
+++ code.h
@@ -1,33 +1,40 @@
 /* Represents an allowed addressing type */
-typedef enum addressing_types {
+typedef enum addressing_types_ARE {
 	/* Immediate addressing (0) */
-	IMMEDIATE = 0,
+	IMMEDIATE = 1 << 2, /* |1|0|0| */
 	/* Direct addressing (1) */
-	DIRECT,
+	DIRECT = 1, /* |0|0|1| */
 	/* Relative addressing (2) */
-	RELATIVE,
-	/* Register (3) */
-	REGISTER
-} addressing_types ;
+	RELATIVE = (1 << 2), /* |1|0|0| */
+	/* command word's are is defaulted to |1|0|0| */
+	COMMAND = 1 << 2
+} addressing_types_ARE ;
 
 /* definition for each command opcode */
 typedef enum opcode {
 	MOV_OP = 0,
 	CMP_OP = 1,
+
 	ADD_OP = 2,
 	SUB_OP = 2,
+
 	LEA_OP = 4,
+
 	CLR_OP = 5,
 	NOT_OP = 5,
 	INC_OP = 5,
 	DEC_OP = 5,
+
 	JMP_OP = 9,
 	BNE_OP = 9,
 	JSR_OP = 9,
+
 	RED_OP = 12,
 	PRN_OP = 13,
 	RTS_OP = 14,
-	STOP_OP = 15
+	STOP_OP = 15,
+
+	NONE_OP = -1
 } opcode;
 
 /* definition for the value of funct, for different commands with same names */
@@ -45,15 +52,15 @@ typedef enum funct {
 	/* OPCODE 9 */
 	JMP_FUNCT = 1,
 	BNE_FUNCT = 2,
-	JSR__FUNCT = 3
+	JSR_FUNCT = 3,
+
+	NONE_FUNCT = -1
 } funct;
 
 /* Represents a single code word */
 typedef struct code_word {
 	/* First byte: ARE+funct */
-	char A:1;
-	char R:1;
-	char E:1;
+	char ARE:3;
 	char funct:5;
 	/* Second byte: destination+addressing, source */
 	char dest_register:3;
@@ -66,13 +73,15 @@ typedef struct code_word {
 
 /* Represents a single data word. */
 typedef struct data_word {
-	char A:1;
-	char R:1;
-	char E:1;
+	char ARE:3;
 	/* The data content itself (a method for putting data into these field is defined) */
 	char data0:5;
 	char data1:8;
 	char data2:8;
 
 } data_word;
+
+/* Processes a code line in first pass */
 int process_code(char *line, int i);
+/* Puts the opcode and the funct values inside the arguments by the name of the command */
+void get_opcode_func(char* cmd, opcode *opcode_out, funct *funct_out);

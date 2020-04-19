682f1b59a69ae8c7eff203edbe85ca251f140826
Aviv Naaman <avivnaaman04@gmail.com>
2020-04-19 20:38:08 +0300

    Assertion errors are now printed into stderr

--- UnitTests/assertions.c
+++ UnitTests/assertions.c
@@ -8,42 +8,42 @@
 int assert_true(int value, const char *description, int *total, int *failed) {
 	(*total)++;
 	if (!value) {
-		printf("\nASSERTION FAILED! on %s. Expected TRUE but got FALSE.", description);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected TRUE but got FALSE.", description);
 		(*failed)++;
 	}
 }
 int assert_false(int value, const char *description, int *total, int *failed){
 	(*total)++;
 	if (value) {
-		printf("\nASSERTION FAILED! on %s. Expected FALSE but got TRUE.", description);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected FALSE but got TRUE.", description);
 		(*failed)++;
 	}
 }
 int assert_int(int expected, int actual, const char *description, int *total, int *failed){
 	(*total)++;
 	if (expected != actual) {
-		printf("\nASSERTION FAILED! on %s. Expected %d but got %d.", description, expected, actual);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected %d but got %d.", description, expected, actual);
 		(*failed)++;
 	}
 }
 int assert_double(double expected, double actual, const char *description, int *total, int *failed){
 	(*total)++;
 	if (expected != actual) {
-		printf("\nASSERTION FAILED! on %s. Expected %f but got %f.", description, expected, actual);;
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected %f but got %f.", description, expected, actual);;
 		(*failed)++;
 	}
 }
 int assert_char(char expected, char actual, const char *description, int *total, int *failed){
 	(*total)++;
 	if (expected != actual) {
-		printf("\nASSERTION FAILED! on %s. Expected %c(%d) but got %c(%d).", description, expected,expected, actual, actual);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected %c(%d) but got %c(%d).", description, expected,expected, actual, actual);
 		(*failed)++;
 	}
 }
 int assert_str(char *expected, char *actual, const char *description, int *total, int *failed){
 	(*total)++;
 	if (strcmp(expected, actual) != 0) {
-		printf("\nASSERTION FAILED! on %s. Expected '%s' but got '%s'.\n", description, expected, actual);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Expected '%s' but got '%s'.\n", description, expected, actual);
 		(*failed)++;
 	}
 }
@@ -51,7 +51,7 @@ int assert_str(char *expected, char *actual, const char *description, int *total
 int assert_null(void *ptr, const char *description, int *total, int *failed) {
 	(*total)++;
 	if (ptr != NULL) {
-		printf("\nASSERTION FAILED! on %s. Pointer *ptr should be NULL.\n", description);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. Pointer *ptr should be NULL.\n", description);
 		(*failed)++;
 	}
 }
@@ -59,7 +59,7 @@ int assert_null(void *ptr, const char *description, int *total, int *failed) {
 int assert_word(char *expected, char *actual, const char *description, int *total, int *failed) {
 	(*total)++;
 	if (expected[0] != actual[0] || expected[1] != actual[1] || expected[2] != actual[2]) {
-		printf("\nASSERTION FAILED! on %s. should be %d,%d,%d but got %d,%d,%d",description,expected[0],expected[1],expected[2],actual[0],actual[1],actual[2]);
+		fprintf(stderr, "\nASSERTION FAILED! on %s. should be %d,%d,%d but got %d,%d,%d",description,expected[0],expected[1],expected[2],actual[0],actual[1],actual[2]);
 		(*failed)++;
 	}
 }
